cd4c7916e80311a803576d0f21bd689f32a17418
Nivas1177 <126694870+Nivas1177@users.noreply.github.com>
2023-06-05 10:04:54 +0530

    n = int(input()) lst = list(map(str,input().split())) l1 = [] for i in lst:     if "-" in i:         l1.append(len(i)-1)     else:         l1.append(len(i)) k = max(l1) print(l1.count(k))

---
+++ Max_digits_count.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = list(map(str,input().split()))
+l1 = []
+for i in lst:
+    if "-" in i:
+        l1.append(len(i)-1)
+    else:
+        l1.append(len(i))
+k = max(l1)
+print(l1.count(k))
